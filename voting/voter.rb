90674daba9bf834dece52640b9751f23aaa12395
Amar Singh <ummerr@gmail.com>
2014-12-24 13:17:26 -0500

    added a layout erb file

--- voting/voter.rb
+++ voting/voter.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 
 get '/' do
+  @title = 'Voting App Live!'
   erb :index
+end
+
+post '/cast' do
+  @title = 'Thanks for casting your vote'
+  @vote = params['vote']
+  erb :cast
 end
 
 Choices = {
